13befcc1ba3913132eb40251159236650dbfe916
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-08-11 13:12:05 +0530

    ok

---
+++ react-classes/class-5/message.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Message extends React{
+
+    msg="Hello";
+
+    render() {
+        return <div>
+            <h2>Message Component</h2>
+            <h3>Message:{this.msg}</h3>
+            <button> onclick={ }GM</button>
+        </div>
+    }
+}
+export default Message;
