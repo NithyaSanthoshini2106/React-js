8ca7889b61287c85677417c48a2fb0047090d2f7
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-08-19 14:41:54 +0530

    ok

--- react31/src/App.js
+++ react31/src/App.js
@@ -1,15 +1,19 @@
 
 
 
-import React from 'react'
-//import '../node_modules/bootstrap/dist/css/bootstrap.css' 
-import Employee from './Employees/Employee'
-import Navbar from './Navbar/Navbar'
-const App = () => {
- return <div>
-            <Navbar/>
-            <Employee/>
-        </div>
-}
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Navbar from "./Users/user";
 
-export default App
+const App = () => {
+  return (
+    <div>
+      <h2>App Component</h2>
+      <hr />
+      <button className="btn btn-success">Test</button> 
+      <Navbar />
+    </div>
+  );
+};
+
+export default user
